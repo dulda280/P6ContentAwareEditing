f6afd1137cedcf4bf969e8c3596ff7af9584c02f
dulda280 <Sebber280@live.dk>
2022-04-01 14:43:36 +0200

    reeee these damn edges

--- EdgeDetection/edgeDetection.py
+++ EdgeDetection/edgeDetection.py
@@ -11,18 +11,18 @@ def makeImageFolder():
     pathDir = os.listdir(path)
     print('her er directory: ', pathDir)
     images = []
-    test = cv2.imread(str(path) + '\\' + str(pathDir[0]), cv2.IMREAD_COLOR)
-    # cv2.imshow(f'test', test)
-    # cv2.waitKey(0)
+
     for image in range(0, len(pathDir)):
-        print("image: ", pathDir[image])
+        # print("image: ", pathDir[image])
+        # print("path: ", str(path) + '\\' + str(pathDir[image]))
+        # print("next image: ", pathDir[image])
+        # print("len", len(pathDir))
 
         temp = cv2.imread(str(path) + '\\' + str(pathDir[image]), cv2.IMREAD_COLOR)
         images.append(temp)
-        cv2.imshow(f'test', temp)
-        cv2.waitKey(0)
 
     return images
+
 
 def makeImagesGrayscale(imageDir):
     grayImages = []
@@ -32,10 +32,34 @@ def makeImagesGrayscale(imageDir):
 
     return grayImages
 
+
+def doGausBlur(imageDir):
+    gausImages = []
+    for image in imageDir:
+        gausImg = cv2.GaussianBlur(src=image, ksize=(3, 3), sigmaX=0, sigmaY=0)
+        gausImages.append(gausImg)
+
+    return gausImages
+
+
+def fuckdigCannybitch(imageDir):
+    cannyBitch = []
+    for index in range(0, len(imageDir)):
+        ree = rescale_image(imageDir[index], 64, 64)
+        canny1B = cv2.Canny(ree, threshold1=100, threshold2=200)
+        cv2.imshow(f'cannybitch', canny1B)
+        cv2.waitKey(0)
+        cannyBitch.append(canny1B)
+
+    return cannyBitch
+
+
 def doSobelHori(imageDir):
     horizontalSobel = []
     for index in range(0, len(imageDir)):
-        sobely = cv2.Sobel(src=imageDir[index], ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5) #Sobel edge detection y-axis
+        reScaled = rescale_image(imageDir[index], 512, 512)
+        sobely = cv2.Sobel(src=reScaled, ddepth=cv2.CV_16S, dx=0, dy=1, ksize=5)  # Sobel edge detection y-axis
+
         horizontalSobel.append(sobely)
 
     return horizontalSobel
@@ -44,24 +68,37 @@ def doSobelHori(imageDir):
 def doSobelVert(imageDir):
     verticalSobel = []
     for index in range(0, len(imageDir)):
-        sobelx = cv2.Sobel(src=imageDir[index], ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5)
+        reScaled = rescale_image(imageDir[index], 512, 512)
+        sobelx = cv2.Sobel(src=reScaled, ddepth=cv2.CV_16S, dx=1, dy=0, ksize=5)
         verticalSobel.append(sobelx)
+
     return verticalSobel
+
+
+def rescale_image(image, res_x, res_y):
+    rescale_dimensions = (res_y, res_x)
+    rescaled_image = cv2.resize(image, rescale_dimensions, interpolation=cv2.INTER_AREA)
+    rescaled_image = cv2.cvtColor(rescaled_image, cv2.COLOR_BGR2RGB)
+    return rescaled_image
+
 
 if __name__ == "__main__":
     imageData = makeImageFolder()
     grayScaleImages = makeImagesGrayscale(imageData)
-    vertImg = doSobelVert(grayScaleImages)
-    horiImg = doSobelHori(grayScaleImages)
+    preProcessImg = doGausBlur(grayScaleImages)
+
+    vertImg = doSobelVert(preProcessImg)
+    horiImg = doSobelHori(preProcessImg)
+
     for image in vertImg:
         cv2.imshow(f'vertImg', image)
         cv2.waitKey(0)
-        print(vertImg[0])
+        print(image)
 
     for image in horiImg:
         cv2.imshow(f'HoriImg', image)
         cv2.waitKey(0)
-        print(horiImg[0])
+        print(image)
 
 # # Sobel Edge Detection
 # sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5) # Sobel Edge Detection on the X axis
@@ -75,5 +112,3 @@ if __name__ == "__main__":
 # cv2.waitKey(0)
 # cv2.imshow('Sobel X Y using Sobel() function', sobelxy)
 # cv2.waitKey(0)
-
-
