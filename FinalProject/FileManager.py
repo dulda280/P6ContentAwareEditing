b0fbe69d20ef4cdc557f527d7f28482d49ed0008
Thomas S.T. Andersen <tsta19@student.aau.dk>
2022-04-20 15:05:37 +0200

    Added DataGenerator Script for Easy Data Construction

--- FinalProject/FileManager.py
+++ FinalProject/FileManager.py
@@ -10,6 +10,13 @@ class FileManager:
     def save_image(self, path, image, keyword, index):
         cv.imwrite(os.path.join(path, str(index) + "_" + str(keyword) + ".jpg"), image)
 
+    def save_image_nokeyword(self, path, image, index):
+        cv.imwrite(os.path.join(path, str(index) + ".jpg"), image)
+
     def save_array(self, path, image, keyword, index):
         with open(os.path.join(path, str(index) + "_" + str(keyword) + ".txt"), 'w') as file:
             file.write(str(image))
+
+    def save_text_file(self, path, value):
+        with open(os.path.join(path, "Gamma_Values.txt"), "a+") as file:
+            file.write(str(value) + "\n")
