c827ad4eeef18a6da228adf37a1554610db238aa
tromborg <ktromb19@student.aau.dk>
2022-04-28 15:06:40 +0200

    Portrait data
    Portrait data

--- FinalProject/DataGenerator.py
+++ FinalProject/DataGenerator.py
@@ -30,7 +30,7 @@ class DataGenerator:
         t = 2
 
         # Index Iterator to determine image number
-        index = 51
+        index = 327
 
         print("+-----------------------------------+")
         print("| Data Generation Started:")
