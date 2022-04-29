b759660e797818d01eebda4e0239c4a3ff6f2c28
Thomas S.T. Andersen <tsta19@student.aau.dk>
2022-04-29 14:24:52 +0200

    Added some Kmeans clustering

--- Thomas/ImageSegmentation.py
+++ Thomas/ImageSegmentation.py
@@ -1,3 +1,4 @@
+from msilib.schema import File
 from keras.preprocessing import image
 from keras.applications.vgg16 import VGG16
 from keras.applications.vgg16 import preprocess_input
@@ -5,16 +6,19 @@ import numpy as np
 from sklearn.cluster import KMeans
 import os, shutil, glob, os.path
 from PIL import Image as pil_image
-image.LOAD_TRUNCATED_IMAGES = True 
-model = VGG16(weights='imagenet', include_top=False)
+image.LOAD_TRUNCATED_IMAGES = False 
+model = VGG16(weights='imagenet', include_top=True)
+from FileManager import *
+
+fileManager = FileManager()
 
 # Variables
-imdir = 'Thomas//image_directory'
-targetdir = "Thomas//save_directory"
+imdir = "Thomas//images//image_directory"
+targetdir = "Thomas//images//save_directory"
 number_clusters = 2
 
 # Loop over files and get features
-filelist = glob.glob(os.path.join(imdir, '*.jpg'))
+filelist = glob.glob(os.path.join(imdir, ('*.jpg' or '*.jpeg')))
 filelist.sort()
 featurelist = []
 for i, imagepath in enumerate(filelist):
@@ -39,4 +43,5 @@ except OSError:
 print("\n")
 for i, m in enumerate(kmeans.labels_):
     print("    Copy: %s / %s" %(i, len(kmeans.labels_)), end="\r")
-    shutil.copy(filelist[i], targetdir + str(m) + "_" + str(i) + ".jpg")
+    #fileManager.save_image(targetdir, filelist[i], "cluster_group", str(i))
+    shutil.copy(filelist[i], targetdir + "cluster_group_" + str(m) + "_" + str(i) + ".jpg")
